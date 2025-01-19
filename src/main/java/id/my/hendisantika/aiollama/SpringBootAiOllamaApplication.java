f89ee48aefde849ba80e3b5309bae4bb78074cbc
hendisantika <hendisantika@yahoo.co.id>
2025-01-19 22:13:26 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/aiollama/SpringBootAiOllamaApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.aiollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAiOllamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAiOllamaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/aiollama/SpringBootAiOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.aiollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAiOllamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
